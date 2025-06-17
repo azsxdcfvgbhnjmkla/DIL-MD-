f965232935437e942af29bce963f73a509d24ef4
dilstar <partinerlover@gmail.com>
2025-06-17 21:51:47 +0300

    Update session.js

--- session.js
+++ session.js
@@ -1,17 +1,7 @@
-{
-  "clientID": "some-unique-id",
-  "serverToken": "your-server-token",
-  "clientToken": "your-client-token",
-  "encKey": "your-encryption-key",
-  "macKey": "your-mac-key",
-  "browserToken": "your-browser-token",
-  "signalIdentities": [
-    {
-      "identifier": {
-        "name": "your-whatsapp-number",
-        "deviceId": 0
-      },
-      "identifierKey": "your-identifier-key"
-    }
-  ]
-}
+
+const { useSingleFileAuthState } = require('@adiwajshing/baileys');
+
+const { state, saveState } = useSingleFileAuthState('./auth.json');
+
+module.exports = { state, saveState };
+
