7394b813e0a4958f9977bdaeb65f6019d4d90c61
dilstar <partinerlover@gmail.com>
2025-06-17 21:43:17 +0300

    Create session.js

---
+++ session.js
@@ -0,0 +1,17 @@
+{
+  "clientID": "some-unique-id",
+  "serverToken": "your-server-token",
+  "clientToken": "your-client-token",
+  "encKey": "your-encryption-key",
+  "macKey": "your-mac-key",
+  "browserToken": "your-browser-token",
+  "signalIdentities": [
+    {
+      "identifier": {
+        "name": "your-whatsapp-number",
+        "deviceId": 0
+      },
+      "identifierKey": "your-identifier-key"
+    }
+  ]
+}
