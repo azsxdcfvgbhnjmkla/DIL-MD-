d56a63d7ce2b7736cc33589157749476dae27ce2
dilstar <partinerlover@gmail.com>
2025-06-21 14:56:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
+const result = `🏓 𝑷𝑰𝑵𝑮 𝑹𝑬𝑺𝑷𝑶𝑵𝑺𝑬  
+💡 Speed: *speedms*  
+⏱ Uptime: *{formatUptime(uptime)}*  
+📅 Date: *moment().format('YYYY-MM-DD')*  
+🕒 Time: *{moment().format('HH:mm:ss')}*  
+
+🖼️ Banner:  
+https://files.catbox.moe/tc788a.jpg
+
+📢 WhatsApp Channel:  
+https://whatsapp.com/channel/0029Vb5vW8yLikgAkyNxnY1H`;
+
 require('dotenv').config();
 const mode = process.env.BOT_MODE || 'public';
 if (mode === 'private') {
