a57a31ce5e5d22ef250f77a9efd984a48c06cd36
dilstar <partinerlover@gmail.com>
2025-06-21 12:47:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const allowGroupCommands = process.env.ALLOW_GROUP_COMMANDS === 'true';
+
 
 require('dotenv').config();
 const AUTO_READ_INBOX = process.env.AUTO_READ_INBOX === 'true';
