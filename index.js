46a3b03263e5ef8df140a15213933d119f1c3878
dilstar <partinerlover@gmail.com>
2025-06-21 15:17:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+require('dotenv').config()
+const settingsMenu = `
+✨ 𝐃𝚰𝐋 - 𝚳𝐃 Bot Settings ✨
+🔘 Auto Read Inbox: process.env.AUTO_READ_INBOX === 'true' ? '✅ On' : '❌ Off'
+🔘 Allow Reactions:{process.env.ALLOW_REACTIONS === 'true' ? '✅ On' : '❌ Off'}
+🔘 Anti-Link Remove: process.env.ANTILINK_REMOVE === 'true' ? '✅ On' : '❌ Off'
+🔘 Delete Links:{process.env.DELETE_LINKS === 'true' ? '✅ On' : '❌ Off'}
+🔘 Allow Group Commands: process.env.ALLOW_GROUP_COMMANDS === 'true' ? '✅ On' : '❌ Off'
+🔘 Fake Typing:{process.env.FAKE_TYPING === 'true' ? '✅ On' : '❌ Off'}
+🔘 Fake Recording: process.env.FAKE_RECORDING === 'true' ? '✅ On' : '❌ Off'
+🔘 Fake Online:{process.env.FAKE_ONLINE === 'true' ? '✅ On' : '❌ Off'}
+🔘 Bot Mode: ${process.env.BOT_MODE || 'private'}
+📺 Channel: https://whatsapp.com/channel/0029Vb5vW8yLikgAkyNxnY1H
+`console.log(settingsMenu)
 
 const result = `🏓 𝑷𝑰𝑵𝑮 𝑹𝑬𝑺𝑷𝑶𝑵𝑺𝑬  
 💡 Speed: *speedms*  
