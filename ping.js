e8b382e12940c7fef134b5d8b43fe333a24a7236
dilstar <partinerlover@gmail.com>
2025-06-21 14:54:21 +0300

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,11 @@
+const result = `🏓 𝑷𝑰𝑵𝑮 𝑹𝑬𝑺𝑷𝑶𝑵𝑺𝑬  
+💡 Speed: *speedms*  
+⏱ Uptime: *{formatUptime(uptime)}*  
+📅 Date: *moment().format('YYYY-MM-DD')*  
+🕒 Time: *{moment().format('HH:mm:ss')}*  
+
+🖼️ Banner:  
+https://files.catbox.moe/tc788a.jpg
+
+📢 WhatsApp Channel:  
+https://whatsapp.com/channel/0029Vb5vW8yLikgAkyNxnY1H`;
